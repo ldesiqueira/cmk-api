054c96d8adb5e3ccc3e02672d48f1abfdddd2558
Mark Heily <mark.heily@bronto.com>
2014-08-20 21:59:20 -0400

    checkpoint

--- lib/check_mk.rb
+++ lib/check_mk.rb
@@ -1,15 +1,76 @@
 class Check_MK
-  require 'check_mk/wato'
+  require 'uri'
+  require 'net/http'
+  require 'pp'
 
-  # Return a list of all the sites on this machine
-  def sites
-    Dir.glob('/opt/omd/sites/*').sort.map { |x| File.basename(x) }
+  # Add a host to a folder
+  # [+name+] the name of the host
+  # [+options+] additional options, such as tags and folders
+  def add_host(name, folder = '', options = {})
+# possible options:
+#       http://localhost/watotest/check_mk/wato.py?
+#    filled_in=edithost
+#    &_transid=-1
+#    &host=thehostname
+#    &contactgroups_use=on
+#    &attr_alias=
+#    &attr_ipaddress=
+#    &parents_0=
+#    &attr_tag_agent=cmk-agent%7Ctcp
+#    &attr_tag_criticality=prod
+#    &attr_tag_networking=lan
+#    &save=Save+%26+Finish
+#    &folder=folder1
+#    &mode=newhost
+
+    params = {
+      filled_in: 'edithost',
+      _transid: '-1',
+      host: name,
+      attr_tag_agent: 'cmk-agent%7Ctcp',
+      attr_tag_networking: 'lan',
+      save: 'Save+%26+Finish',
+      folder: folder,
+      mode: 'newhost',
+      _do_confirm: 'yes',
+      _do_actions: 'yes',
+      }
+    params.merge! options
+
+    http_request('http://localhost/watotest/check_mk/wato.py', params)
   end
 
-  # Return a handle to a specific site
-  # [+name+] the name of the site
-  def site(name)
-    raise ArgumentError, 'site not found' unless sites.include? name
-    Check_MK::Wato.new('/opt/omd/sites/' + name)
+  def delete_host(name, folder = '')
+    # example:
+    #http://localhost/watotest/check_mk/wato.py?mode=folder&_delete_host=testhost&_transid=1408580836/1995573565&folder=folder1&_do_confirm=yes 
+    http_request('http://localhost/watotest/check_mk/wato.py', {
+    	mode: 'folder',
+	_delete_host: name,
+	_transid: '-1',
+	folder: folder,
+	_do_confirm: 'yes',
+	_do_actions: 'yes',
+    })
+  end
+
+  def activate
+    http_request('http://localhost/watotest/check_mk/wato_ajax_activation.py', {})
+  end
+
+  private
+
+  def http_request(request_uri, params, debug = false)
+    uri = URI.parse(request_uri)
+    uri.query = URI.encode_www_form(params)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.basic_auth("cmk-api", "cmk-api-secret")
+    response = http.request(request)
+    if debug
+      puts "uri: " + uri.to_s
+      pp response
+      puts response.body
+    end
+    response.value
   end
 end
